c2409c5357ba157b5f1a623825455a67f847c6bc
aiu652106-create <aiu652106-create@users.noreply.github.com>
2026-08-04 05:15:02 +0900

    Keep article title inside post edge

--- functions/news/[slug].js
+++ functions/news/[slug].js
@@ -311,7 +311,7 @@ export async function onRequestGet(context) {
   ${image ? `<meta name="twitter:image" content="${escapeHtml(image)}">` : ""}
   <title>${escapeHtml(article.title)} | ${escapeHtml(siteName)}</title>
   <script type="application/ld+json">${structuredData}</script>
-  <link rel="stylesheet" href="/styles.css?v=20260804-affiliate-layout7">
+  <link rel="stylesheet" href="/styles.css?v=20260804-affiliate-layout8">
   <script src="/config.js" defer></script>
   <script src="/site.js" defer></script>
   <script src="/comments.js" defer></script>
